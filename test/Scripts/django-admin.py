0c3638c7fb8b05c83bfd4ec645945929a57dd70c
udonwa10 <umohu67@gmail.com>
2020-06-08 11:13:32 -0700

    initial commit

---
+++ test/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\UDONWA\Python_works\django_social_app\test\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
